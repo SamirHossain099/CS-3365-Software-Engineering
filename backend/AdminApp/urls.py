6c571c82c3b5a39822d2330c2192d3420d1a0808
Samir Hossain <samirhossain099@gmail.com>
2024-11-09 00:57:56 -0600

    diddy

--- backend/AdminApp/urls.py
+++ backend/AdminApp/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-from .views import (
-    BookingListView,
-    BookingCreateView,
-    BookingsByUserView,
-    BookingCancelView
-)
+from . import views
 
 urlpatterns = [
-    path('', BookingListView.as_view(), name='booking-list'),
-    path('create/', BookingCreateView.as_view(), name='booking-create'),
-    path('user/<int:user_id>/', BookingsByUserView.as_view(), name='bookings-by-user'),
-    path('cancel/<int:booking_id>/', BookingCancelView.as_view(), name='booking-cancel'),
+    path('logs/', views.AdminLogListView.as_view(), name='admin-logs-list'),
+    path('log/create/', views.AdminLogCreateView.as_view(), name='admin-log-create'),
+    path('log/<int:admin_id>/', views.AdminLogsByAdminView.as_view(), name='admin-logs-by-admin'),
 ]
