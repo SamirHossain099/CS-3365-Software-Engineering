d73377e14bb4de1533ad3d7521fe4b8835207736
nrethans <145207516+nrethans@users.noreply.github.com>
2024-11-08 23:39:31 -0600

    movies

--- backend/MovieApp/models.py
+++ backend/MovieApp/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.IntegerField(max_length= 10, primary_key=True)
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    release_date = models.CharField(max_length=100)
+    description = models.TextField()
+    rating = models.FloatField()
+
+    def add_movie(cls, title, genre, duration, release_date, description): # Returns movie_id
+        try:
+            if cls.objects.filter(title=title).exists():
+                return False
+            
+            movie = cls.objects.create(
+                title=title,
+                genre=genre,
+                duration=duration,
+                release_date=release_date,
+                description=description
+            )
+            return True
+        except Exception:
+            return False
+
+    def update_movie(cls, movie_id, **kwargs):
+        try:
+            movie = cls.objects.get(movie_id=movie_id)
+            for key, value in kwargs.items():
+                setattr(movie, key, value)
+            movie.save()
+            return True
+        except cls.DoesNotExist:
+            return False
+
+    def delete_movie(cls, movie_id):
+        try:
+            movie = cls.objects.get(movie_id=movie_id)
+            movie.delete()
+            return True
+        except cls.DoesNotExist:
+            return False
+        
+    def get_movie_details(movie_id):
+        try:
+            movie = Movie.objects.get(movie_id=movie_id)
+            return {
+                'movie_id': movie.movie_id,
+                'title': movie.title,
+                'genre': movie.genre,
+                'duration': movie.duration,
+                'release_date': movie.release_date,
+                'description': movie.description,
+                'rating': movie.rating
+            }
+        except Movie.DoesNotExist:
+            return None
+
+    def update_rating(movie_id, new_rating):
+        try:
+            movie = Movie.objects.get(movie_id=movie_id)
+            movie.rating = new_rating
+            movie.save()
+            return True
+        except Movie.DoesNotExist:
+            return False
--- backend/ReviewApp/models.py
+++ backend/ReviewApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    review_id = models.IntegerField(primary_key=True)
+    user_id = models.IntegerField(max_length=10)
+    movie_id = models.IntegerField(max_length=10)
+    rating = models.FloatField()
+    review = models.TextField(max_length=1000)
+    review_date = models.DateField()
+
+    def add_review(user_id, movie_id, rating, review): int # Returns review_id
+    def get_reviews_by_movie(movie_id): list
+    def update_movie_review(review_id, **kwargs): bool
