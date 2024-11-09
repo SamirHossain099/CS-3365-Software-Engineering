739a86c418d35741c4beffc4b592f72cb46299cc
Wesley Spangler <wesspangler98@gmail.com>
2024-11-08 22:37:39 -0600

    update

--- backend/mainApp/urls.py
+++ backend/mainApp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('theaters/', views.theaters, name='theaters'),
+    path('movies/', views.movies, name='movies'),
+    path('purchases/', views.receipts, name='purchases'),
 ]
